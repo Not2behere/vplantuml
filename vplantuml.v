567db03190f979ca7277309d82730d7486f4a78e
Not2behere <not2behere@gmx.fr>
2022-03-26 16:07:27 +0100

    Update

--- vplantuml.v
+++ vplantuml.v
@@ -1,5 +1,22 @@
 module main
+import json
+import rand
+import net.http
+
+
 
 fn main() {
 	println('Hello World!')
+
+	config := http.FetchConfig{
+		user_agent: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+	}
+
+	url := 'http://www.plantuml.com/plantuml/img/'
+	println(url)
+
+	resp := http.fetch(http.FetchConfig{ ...config, url: url }) or {
+		println('failed to fetch data from the server')
+		return
+	}
 }
