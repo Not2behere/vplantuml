aa84ad8289b2ffa577261f3f49269a1aad889407
Not2behere <not2behere@gmx.fr>
2022-04-03 12:32:18 +0200

    Modified comment

--- vplantuml.v
+++ vplantuml.v
@@ -45,7 +45,7 @@ fn get_diagram(cmd Command) ? {
 	plantuml_file := cmd.flags.get_string('file') or {
 		panic('Failed to get `Plantuml file` flag: $err')
 	}
-	out_bool := cmd.flags.get_bool('output') ? // This one is optional for output file
+	out_bool := cmd.flags.get_bool('output') ? // Optional for output file
 
 	mut plantuml_text := ''
 	if plantuml_string != [] {
