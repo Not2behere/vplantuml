0c609e650a1ac59aeed371af36c0afe476cde815
Not2behere <not2behere@gmx.fr>
2022-04-03 12:30:23 +0200

    Update tests

--- vplantuml_test.v
+++ vplantuml_test.v
@@ -2,10 +2,10 @@ module main
 
 fn test_deflate() {
 	plantuml_text := '@startuml\nAlice -> Bob: Authentication Request\nBob --> Alice: Authentication Response\n@enduml'
-	assert get_deflate_url(plantuml_text) == 'http://www.plantuml.com/plantuml/svg/1K2n3O0W4EoPuXTW0GjZBA5Z0ubVaC2_Ud_xcmDnVS4rKo5YcEHTJcTtccvI1o1DNoeYdSxkD9MSTob43Dka3a2Qud63faFjvfe_'
+	assert get_deflate_url(plantuml_text) == 'http://www.plantuml.com/plantuml/svg/1K313O0W3FmpHHTW0Hz67C4D42-n2RGgr_tDDba_nkYfT6sGlChkvo8bUCEehLBTvC0Rc4oxdvBpAgLhmo8bUCEehLBTvC1yt2RI1hjYz1y0'
 }
 
 fn test_hex() {
 	plantuml_text := '@startuml\nAlice -> Bob: Authentication Request\nBob --> Alice: Authentication Response\n@enduml'
-	assert get_hex_url(plantuml_text) == 'http://www.plantuml.com/plantuml/svg/~h407374617274756D6C0A4A757374696E202D3E20426F75626F756E653A204B69737320526571756573740A426F75626F756E65202D2D3E204A757374696E3A204B69737320526573706F6E73650A40656E64756D6C'
+	assert get_hex_url(plantuml_text) == 'http://www.plantuml.com/plantuml/svg/~h407374617274756D6C0A416C696365202D3E20426F623A2041757468656E7469636174696F6E20526571756573740A426F62202D2D3E20416C6963653A2041757468656E7469636174696F6E20526573706F6E73650A40656E64756D6C'
 }
